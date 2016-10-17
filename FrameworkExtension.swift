e793d35e5101bc606af69077d7963324fc1a34ac
IamAlchemist <Wizard.Lee@qq.com>
2016-10-17 17:10:08 +0800

    mc

---
+++ FrameworkExtension.swift
@@ -0,0 +1,21 @@
+//
+//  FrameworkExtension.swift
+//  ZaChatiOS
+//
+//  Created by wizard lee on 10/17/16.
+//  Copyright © 2016 zhongan. All rights reserved.
+//
+
+import UIKit
+
+extension Dictionary {
+    var json: String {
+        let invalidJson = "Not a valid JSON"
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: self, options: .prettyPrinted)
+            return String(bytes: jsonData, encoding: String.Encoding.utf8) ?? invalidJson
+        } catch {
+            return invalidJson
+        }
+    }
+}
