6002bea900c281f4e7a94e75fb2a6dd9b302c8aa
IamAlchemist <Wizard.Lee@qq.com>
2016-10-27 11:50:04 +0800

    mc

--- Util.swift
+++ Util.swift
@@ -96,5 +96,34 @@ struct MWUtil {
         viewController.present(alertController, animated: true, completion: nil)
         
     }
+    
+    static func setTitle(color: UIColor, for navigationBar: UINavigationBar?) {
+        guard let navigationBar = navigationBar else { return  }
+        var dict = navigationBar.titleTextAttributes ?? [String:Any]()
+        dict[NSForegroundColorAttributeName] = color
+        navigationBar.titleTextAttributes = dict
+    }
+    
+    static func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
 }
 
