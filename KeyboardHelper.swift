407fe26690c1dcb24240fc24cf29f968542502c3
IamAlchemist <Wizard.Lee@qq.com>
2016-10-28 17:22:54 +0800

    mc

--- KeyboardHelper.swift
+++ KeyboardHelper.swift
@@ -37,56 +37,61 @@ class KeyboardHelper {
     
     @objc func keyboardWillShow(_ notification: Notification) {
         if let rect = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            originInset = scrollView.contentInset
+            var inset = scrollView.contentInset
+            inset.bottom += rect.height
+            scrollView.contentInset = inset
             
-            let padding = max(scrollView.frame.height - scrollView.contentInset.top - scrollView.contentInset.bottom - scrollView.contentSize.height, 0)
-            
-            printDebug("padding = \(padding), contentSize: \(scrollView.contentSize)")
-            
-            let scrollVisibleArea = scrollView.convert(scrollView.frame, to: rootView)
-            
-            printDebug("scroll frame in root view : \(scrollVisibleArea)")
-            
-            let rootViewVisible = CGRect(x: rootView.bounds.origin.x,
-                                         y: rootView.bounds.origin.y,
-                                         width: rootView.bounds.width,
-                                         height: rootView.bounds.height)
-            
-            printDebug("root visible area : \(rootViewVisible)")
-            
-            printDebug("keyboard size : \(rect)")
-            
-            var visibleArea = rootViewVisible.intersection(scrollVisibleArea)
-            
-            printDebug("intersected keyboard size : \(visibleArea)")
-            
-            visibleArea = CGRect(origin: visibleArea.origin, size: CGSize(width: visibleArea.width, height: visibleArea.height - rect.height))
-            
-            printDebug("visible area : \(visibleArea)")
-            
-            let inputArea = self.inputArea.convert(self.inputArea.bounds, to: rootView)
-            
-            printDebug("input area : \(inputArea)")
-            
-            if !visibleArea.contains(inputArea) {
-                
-                var inset = scrollView.contentInset
-                originInset = inset
-                
-                printDebug("origin inset : \(inset)")
-                
-                printDebug("diff is : \(inputArea.maxY - visibleArea.maxY)")
-                inset.bottom += inputArea.maxY - visibleArea.maxY + padding + bottomMargin
-                
-                scrollView.contentInset = inset
-                
-                printDebug("scrollview inset : \(scrollView.contentInset)")
-            }
+//            let padding = max(scrollView.frame.height - scrollView.contentInset.top - scrollView.contentInset.bottom - scrollView.contentSize.height, 0)
+//            
+//            printDebug("padding = \(padding), contentSize: \(scrollView.contentSize)")
+//            
+//            let scrollVisibleArea = scrollView.convert(scrollView.frame, to: rootView)
+//            
+//            printDebug("scroll frame in root view : \(scrollVisibleArea)")
+//            
+//            let rootViewVisible = CGRect(x: rootView.bounds.origin.x,
+//                                         y: rootView.bounds.origin.y,
+//                                         width: rootView.bounds.width,
+//                                         height: rootView.bounds.height)
+//            
+//            printDebug("root visible area : \(rootViewVisible)")
+//            
+//            printDebug("keyboard size : \(rect)")
+//            
+//            var visibleArea = rootViewVisible.intersection(scrollVisibleArea)
+//            
+//            printDebug("intersected keyboard size : \(visibleArea)")
+//            
+//            visibleArea = CGRect(origin: visibleArea.origin, size: CGSize(width: visibleArea.width, height: visibleArea.height - rect.height))
+//            
+//            printDebug("visible area : \(visibleArea)")
+//            
+//            let inputArea = self.inputArea.convert(self.inputArea.bounds, to: rootView)
+//            
+//            printDebug("input area : \(inputArea)")
+//            
+//            if !visibleArea.contains(inputArea) {
+//                
+//                var inset = scrollView.contentInset
+//                originInset = inset
+//                
+//                printDebug("origin inset : \(inset)")
+//                
+//                printDebug("diff is : \(inputArea.maxY - visibleArea.maxY)")
+//                inset.bottom += inputArea.maxY - visibleArea.maxY + padding + bottomMargin
+//                
+//                scrollView.contentInset = inset
+//                
+//                printDebug("scrollview inset : \(scrollView.contentInset)")
+//            }
         }
     }
     
     @objc func keyboardWillHide(_ notification: Notification) {
         if let originInset = originInset {
             scrollView.contentInset = originInset
+            self.originInset = nil
         }
     }
     
