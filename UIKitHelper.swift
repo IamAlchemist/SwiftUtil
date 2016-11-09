4043b0b2c99dc4b81bef4f34954bdbd58456b7d3
IamAlchemist <Wizard.Lee@qq.com>
2016-11-09 18:40:16 +0800

    minor refactor

---
+++ UIKitHelper.swift
@@ -0,0 +1,21 @@
+//
+//  UIKitHelper.swift
+//  ZaChatiOS
+//
+//  Created by wizard lee on 11/9/16.
+//  Copyright © 2016 zhongan. All rights reserved.
+//
+
+import UIKit
+
+struct MWUIKitHelper {
+    public static func toolBarFor(action:Selector) -> UIToolbar {
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 44))
+        toolBar.tintColor = UIColor.gray
+        let doneBtn = UIBarButtonItem(title: "完成", style: .done, target: self, action: action)
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolBar.items = [space, doneBtn]
+        
+        return toolBar
+    }
+}
